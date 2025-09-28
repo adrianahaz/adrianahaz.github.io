f84bf9d893698c2f8c410e4fb37de3ea2d269fbe
adrhmdlz <adrianalzidan35@gmail.com>
2025-09-28 11:07:28 +0700

    feat(ui): add Button and Icon component

---
+++ src/lib/components/ui/button/Button.component.svelte
@@ -0,0 +1,15 @@
+<script>
+	let props = $props();
+</script>
+
+<button
+	class="{props.class
+		? props.class
+		: ''} relative rounded-[40px] p-[1px] bg-gradient-to-r from-[#fed39c] to-[#7a87fa] text-black"
+>
+	<span
+		class="block rounded-[40px] bg-gradient-to-r from-[#7a87fa] to-[#fed39c] px-[20px] py-[14px] font-semibold"
+	>
+		{props.label}
+	</span>
+</button>
---
+++ src/lib/components/ui/icon/Icon.component.svelte
@@ -0,0 +1,5 @@
+<script>
+	let props = $props();
+</script>
+
+<props.icon size={props.size ? props.size : 22} />
