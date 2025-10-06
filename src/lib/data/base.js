da918877a987f7d1996decf16bdb4ca7aa2dcfe1
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-07 01:07:25 +0700

    feat(hero): add new hero section to page

--- src/lib/data/base.js
+++ src/lib/data/base.js
@@ -1,8 +1,17 @@
-const items = [
+const navItems = [
 	{ label: 'Home', href: '/', icon: 'Home' },
 	{ label: 'About', href: '#about' },
 	{ label: 'Projects', href: '#projects' },
 	{ label: 'Contact me', href: '#contact' }
 ];
 
-export const getAllNavItems = () => items;
+const marqueeItems = [
+	{ label: 'JavaScript', href: '#' },
+	{ label: 'Svelte', href: '#' },
+	{ label: 'Node.js', href: '#' },
+	{ label: 'Tailwind', href: '#' }
+];
+
+export const getAllNavItems = () => navItems;
+
+export const getAllMarqueeItems = () => marqueeItems;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+	import Hero from '$lib/components/common/hero/Hero.component.svelte';
+	import Marquee from '$lib/components/ui/marquee/Marquee.component.svelte';
+</script>
+
 <svelte:head>
 	<title>Adrian Portfolio | Home</title>
 </svelte:head>
+
+<main>
+	<Hero />
+	<Marquee />
+</main>
