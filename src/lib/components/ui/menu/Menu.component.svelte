1b0bc1eaeb3570412b8dab38baa5331dde8bdaa6
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-07 00:59:34 +0700

    refactor(menu): Refactor menu component from common to ui

--- src/lib/components/ui/menu/Menu.component.svelte
+++ src/lib/components/ui/menu/Menu.component.svelte
@@ -1,7 +1,10 @@
 <script>
-	import Icon from '$lib/components/ui/icon/Icon.component.svelte';
 	import { getIcon } from '$lib/data/icons.js';
 	import { getAllNavItems } from '$lib/data/base.js';
+
+	import Icon from '$lib/components/ui/icon/Icon.component.svelte';
+	import Button from '$lib/components/ui/button/Button.component.svelte';
+	import MenuItem from '$lib/components/ui/menu/MenuItem.component.svelte';
 
 	const navItems = getAllNavItems();
 
@@ -19,18 +22,7 @@
 		class="hidden lg:flex p-[6px] justify-center items-center gap-[40px] bg-[#1C1C1C] rounded-full"
 	>
 		{#each navItems as item}
-			<li>
-				<a
-					href={item.href}
-					class="px-[16px] py-[8px] flex items-center gap-2 hover:bg-[#292929] active:bg-[#292929] rounded-full"
-				>
-					{#if item.icon}
-						<Icon icon={getIcon(item.icon)} />
-					{:else}
-						<span>{item.label}</span>
-					{/if}
-				</a>
-			</li>
+			<MenuItem href={item.href} icon={item.icon} label={item.label} />
 		{/each}
 	</div>
 
@@ -46,19 +38,12 @@
 			class="hidden absolute top-[50px] left-1/2 w-[90vw] -translate-x-1/2 mt-2 bg-[#1C1C1C] rounded-lg shadow-lg p-[20px] z-10 flex-col gap-5"
 			id="mobile-menu"
 		>
-			<li><a href="/">Home</a></li>
-			<li><a href="#about">About</a></li>
-			<li><a href="#services">Services</a></li>
-			<li><a href="#contact">Contact</a></li>
+			{#each navItems as item}
+				<li><a href={item.href}>{item.label}</a></li>
+			{/each}
 			<hr />
 			<li>
-				<a
-					href="https://drive.google.com/file/d/1gk1bX4YHk1F2JH3K5J6K7L8M9N0O1P2Q/view?usp=sharing"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="block w-full text-center bg-gradient-to-r from-[#fed39c] to-[#7a87fa] text-black rounded-[40px] px-[20px] py-[14px] font-semibold"
-					>Resume</a
-				>
+				<Button as="a" label="Resume" href="/resume.pdf" />
 			</li>
 		</div>
 	</div>
---
+++ src/lib/components/ui/menu/MenuItem.component.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { getIcon } from '$lib/data/icons.js';
+
+	import Icon from '$lib/components/ui/icon/Icon.component.svelte';
+
+	let props = $props();
+</script>
+
+<li>
+	<a
+		href={props.href}
+		class="px-[16px] py-[8px] flex items-center gap-2 hover:bg-[#292929] active:bg-[#292929] rounded-full"
+	>
+		{#if props.icon}
+			<Icon icon={getIcon(props.icon)} />
+		{:else}
+			<span>{props.label}</span>
+		{/if}
+	</a>
+</li>
