22a3f2efc668b347e78127ea7e48ab5ca12e193c
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-07 01:02:15 +0700

    feat(button): added option if this button is used as a hyperlink or a regular button

--- src/lib/components/ui/button/Button.component.svelte
+++ src/lib/components/ui/button/Button.component.svelte
@@ -2,14 +2,29 @@
 	let props = $props();
 </script>
 
-<button
-	class="{props.class
-		? props.class
-		: ''} relative rounded-[40px] p-[1px] bg-gradient-to-r from-[#fed39c] to-[#7a87fa] text-black"
->
-	<span
-		class="block rounded-[40px] bg-gradient-to-r from-[#7a87fa] to-[#fed39c] px-[20px] py-[14px] font-semibold"
+{#if props.as === 'a'}
+	<a
+		href={props.href ? props.href : '#'}
+		class="{props.class
+			? props.class
+			: ''} relative rounded-[40px] p-[1px] text-black text-center hover:bg-gradient-to-r from-[#fed39c] to-[#7a87fa]"
 	>
-		{props.label}
-	</span>
-</button>
+		<span
+			class="block rounded-[40px] bg-gradient-to-r from-[#7a87fa] to-[#fed39c] px-[20px] py-[14px] font-semibold"
+		>
+			{props.label}
+		</span>
+	</a>
+{:else}
+	<button
+		class="{props.class
+			? props.class
+			: ''} relative rounded-[40px] p-[1px] bg-gradient-to-r from-[#fed39c] to-[#7a87fa] text-black"
+	>
+		<span
+			class="block rounded-[40px] bg-gradient-to-r from-[#7a87fa] to-[#fed39c] px-[20px] py-[14px] font-semibold"
+		>
+			{props.label}
+		</span>
+	</button>
+{/if}
