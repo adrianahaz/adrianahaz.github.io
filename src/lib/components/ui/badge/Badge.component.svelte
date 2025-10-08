c0a98013d0adee8c06e829a644cf41811e3964b7
adrianahaz <adrianalzidan35@gmail.com>
2025-10-08 20:02:35 +0700

    feat(badge): added badge component

---
+++ src/lib/components/ui/badge/Badge.component.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { getAllIdentityItems } from '$lib/data/base.js';
+
+	import SpecialIcon from '$lib/components/ui/icon/SpecialIcon.component.svelte';
+
+	let identities = getAllIdentityItems();
+</script>
+
+<div
+	class="flex justify-around items-center w-full border-t border-b border-white/10 py-[20px] text-[12px] md:text-[16px]"
+>
+	{#each identities as { label }}
+		<span class="badge">{label}</span>
+		<SpecialIcon />
+	{/each}
+</div>
