62f5f303c63b9ac9129cbca7df01947a6f1e5d82
adrianahaz <adrianalzidan35@gmail.com>
2025-10-12 22:09:08 +0700

    feat(experience): added experience setion and fix some component

---
+++ src/lib/components/ui/timeline/Timeline.component.svelte
@@ -0,0 +1,30 @@
+<script>
+	import TimelineItem from './TimelineItem.component.svelte';
+
+	let props = $props();
+</script>
+
+<div class="timeline">
+	{#each props.experiences as experience}
+		<TimelineItem {...experience} />
+	{/each}
+</div>
+
+<style>
+	.timeline {
+		--gutter: 40px;
+		position: relative;
+		margin: 0;
+	}
+
+	.timeline::before {
+		content: '';
+		position: absolute;
+		left: calc(var(--gutter) / 2);
+		top: 15px;
+		bottom: 0;
+		width: 2px;
+		height: calc(100% - 200px);
+		background: rgba(255, 255, 255, 0.06);
+	}
+</style>
