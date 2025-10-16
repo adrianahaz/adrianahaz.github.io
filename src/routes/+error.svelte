8058d0223564b614ede338f2b99e827c752e8afd
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-16 14:02:48 +0700

    test(error): test

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { page } from '$app/state';
+
+	import Button from '$lib/components/ui/button/Button.component.svelte';
+</script>
+
+<main class="h-[100vh] min-w-full flex justify-center items-center px-6 py-24 sm:py-32 lg:px-8">
+	<div class="text-center">
+		<p class="text-base font-semibold text-indigo-400">{page.status}</p>
+		<h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
+			{page.error?.message ?? 'Something went wrong!'}
+		</h1>
+		<p class="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
+			Sorry, we couldn’t find the page you’re looking for.
+		</p>
+		<div class="mt-10 flex items-center justify-center gap-x-6">
+			<Button as="a" href="/" label="Go back home" />
+			<a href="mailto:adrhmdbusiness@gmail.com" class=" font-semibold text-white"
+				>Contact support <span aria-hidden="true">&rarr;</span></a
+			>
+		</div>
+	</div>
+</main>
