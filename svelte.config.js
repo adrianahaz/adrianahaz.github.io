3edd3f385820ade64ba3262fa9f63e4afc1a6664
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-16 10:24:41 +0700

    feat(404 page): change the old error page

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,10 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			},
 			precompress: false,
 			strict: true
 		})
