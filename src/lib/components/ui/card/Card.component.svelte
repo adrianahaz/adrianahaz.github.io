b6dbed1964bce308eb02ff9980db7951f5fc6ac7
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-19 01:21:31 +0700

    feat(card, slider): added new components, card and slider

---
+++ src/lib/components/ui/card/Card.component.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Button from '$lib/components/ui/button/Button.component.svelte';
+	import { limitTagline } from '$lib/helpers/limitTagline.js';
+
+	let { name, img, description, taglines, repository, demo } = $props();
+	let newTaglines = limitTagline(taglines, 2);
+</script>
+
+<div class="bg-[var(--background-secondary)] p-5 rounded-l flex flex-col gap-3">
+	<img src={img} alt={name} class="rounded-l" />
+
+	<div class="flex flex-col gap-4">
+		<h3>{name}</h3>
+
+		<div class="grid grid-cols-3 gap-1">
+			{#each newTaglines as tagline}
+				<span
+					class="text-center text-xs rounded-full bg-[var(--background)] px-3 py-1 flex justify-center items-center"
+					>{tagline}</span
+				>
+			{/each}
+		</div>
+
+		<p class="text-sm">{description}</p>
+
+		<div class="flex justify-center gap-5">
+			<Button type="secondary" label="Repository" class="md:w-[40%]" href={repository} />
+			<Button type="primary" label="Demo" class="md:w-[40%]" href={demo} />
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/ui/slider/Slider.component.svelte
@@ -0,0 +1,21 @@
+<script>
+	let { sliderName, value = $bindable() } = $props();
+
+	let currentSlider = $state(0);
+
+	function changeSlider(index) {
+		value = index;
+		currentSlider = index;
+	}
+</script>
+
+<div class="border p-1 rounded-full inline-flex gap-2 justify-center items-center">
+	{#each sliderName as label, index}
+		<button
+			onclick={() => changeSlider(index)}
+			class={`${currentSlider === index ? 'bg-[#292929]' : ''} p-2 flex items-center gap-2 hover:bg-[#292929] active:bg-[#292929] rounded-full`}
+		>
+			{label}
+		</button>
+	{/each}
+</div>
