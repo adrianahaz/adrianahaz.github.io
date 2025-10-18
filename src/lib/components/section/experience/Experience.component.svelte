ce71a563c9e3ab601defa01be637e1d942ec65f1
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-19 01:20:42 +0700

    feat(experience): added experience section

---
+++ src/lib/components/section/experience/Experience.component.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Spark from '$lib/components/ui/icon/Spark.component.svelte';
+	import Timeline from '$lib/components/ui/timeline/Timeline.component.svelte';
+
+	let props = $props();
+	let experiences = props.experiences || [];
+</script>
+
+<section class="p-[20px] md:px-[100px] md:py-[50px] flex flex-col gap-[25px] md:gap-[50px]">
+	<div class="flex items-center gap-3">
+		<h2 class="text-l md:text-3xl">My journey until now</h2>
+		<Spark />
+	</div>
+
+	<Timeline {experiences} />
+</section>
