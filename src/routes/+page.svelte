22901d75f5b3b91042f0299e88d649691232c006
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-19 01:22:40 +0700

    feat(section): added new section, experience and project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,31 @@
 <script>
 	import { getAllExperience } from '$lib/data/experience.js';
+	import { getAllProjects, getAllAssignments } from '$lib/data/projects.js';
 
 	import Hero from '$lib/components/section/hero/Hero.component.svelte';
+	import Experience from '$lib/components/section/experience/Experience.component.svelte';
+	import Project from '$lib/components/section/project/Project.component.svelte';
 	import Badge from '$lib/components/ui/badge/Badge.component.svelte';
-	import Timeline from '$lib/components/ui/timeline/Timeline.component.svelte';
-	import Spark from '$lib/components/ui/icon/Spark.component.svelte';
 
 	let experiences = getAllExperience();
+	let projects = getAllProjects();
+	let assignments = getAllAssignments();
 </script>
 
 <svelte:head>
-	<title>Adrianahaz Portfolio</title>
-	<meta name="description" content="Adrian's personal portfolio website." />
+	<title>Adrian's Portfolio</title>
+	<meta
+		name="description"
+		content="Adrian's personal portfolio website. Created with SvelteKit and TailwindCSS."
+	/>
 	<meta name="author" content="Adrian Ahmad Al Zidan" />
 </svelte:head>
 
-<main>
+<main class="md:pb-[100px]">
 	<Hero />
 	<Badge />
 </main>
 
-<section class="p-[20px] md:p-[100px] flex flex-col gap-[25px] md:gap-[50px]">
-	<div class="flex items-center gap-3">
-		<h2 class="text-l md:text-3xl">My journey until now</h2>
-		<Spark />
-	</div>
+<Experience {experiences} />
 
-	<Timeline {experiences} />
-</section>
+<Project {projects} {assignments} />
