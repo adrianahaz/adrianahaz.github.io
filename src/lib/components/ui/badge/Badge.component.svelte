f6ebe8713dbbafd816931819aee1958199b09cb8
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-21 16:53:43 +0700

    refactor(badge): refactoring badge component

--- src/lib/components/ui/badge/Badge.component.svelte
+++ src/lib/components/ui/badge/Badge.component.svelte
@@ -1,17 +1,15 @@
 <script>
-	import { getAllIdentityItems } from '$lib/data/base.js';
-
 	import SpecialIcon from '$lib/components/ui/icon/SpecialIcon.component.svelte';
 
-	let identities = getAllIdentityItems();
+	let { badges } = $props();
 </script>
 
 <div
 	class="flex justify-around items-center w-full border-t border-b border-white/10 py-[20px] text-[10px] md:text-[16px]"
 >
-	{#each identities as { label }, index}
+	{#each badges as { label }, index}
 		<span class="badge">{label}</span>
-		{#if index < identities.length - 1}
+		{#if index < badges.length - 1}
 			<SpecialIcon />
 		{/if}
 	{/each}
