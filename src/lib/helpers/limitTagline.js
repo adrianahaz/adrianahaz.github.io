96677f4e15c73225411f5cee2d5616c2a6d59d35
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-21 16:44:43 +0700

    feat(helper): add helper for limit a tagline

--- src/lib/helpers/limitTagline.js
+++ src/lib/helpers/limitTagline.js
@@ -1,3 +1,10 @@
+/**
+ * Limits the tags to be displayed to n that match the limit entered.
+ *
+ * @param {string[]} taglines - Array contains a list of taglines to be displayed.
+ * @param {number} limit - The maximum number of taglines you want to display.
+ * @returns {string[]} - A limited array of taglines, plus markers if there are any left over.
+ */
 export function limitTagline(taglines, limit) {
 	let visible = taglines.slice(0, limit);
 	let remaining = taglines.length - limit;
