a554c80adcfc10ea3ceac1cabad61769360b07a8
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-21 16:48:39 +0700

    style(experience, projects section): fix styling problem for experience and projects section

--- src/lib/components/section/experience/Experience.component.svelte
+++ src/lib/components/section/experience/Experience.component.svelte
@@ -6,7 +6,9 @@
 	let experiences = props.experiences || [];
 </script>
 
-<section class="p-[20px] md:px-[100px] md:py-[50px] flex flex-col gap-[25px] md:gap-[50px]">
+<section
+	class="px-[20px] py-[40px] md:px-[100px] md:py-[75px] flex flex-col gap-[25px] md:gap-[50px]"
+>
 	<div class="flex items-center gap-3">
 		<h2 class="text-l md:text-3xl">My journey until now</h2>
 		<Spark />
--- src/lib/components/section/project/Project.component.svelte
+++ src/lib/components/section/project/Project.component.svelte
@@ -8,7 +8,7 @@
 	let sliderName = ['Assignments', 'Projects'];
 </script>
 
-<section class="px-[20px] md:px-[100px] md:py-[50px] flex flex-col gap-10">
+<section class="px-[20px] py-[40px] md:px-[100px] md:py-[75px] flex flex-col gap-10">
 	<div class="flex items-center justify-center gap-3">
 		<h2 class="text-l md:text-3xl text-center">Some stuff I'm working on</h2>
 		<Spark />
@@ -29,23 +29,4 @@
 			{/if}
 		</div>
 	</div>
-
-	<!-- <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
-		{#each projects as project (project.title)}
-			<div
-				class="border border-gray-300 dark:border-gray-700 rounded-lg p-5 hover:shadow-lg transition-shadow duration-300"
-			>
-				<h3 class="text-md md:text-xl font-semibold mb-2">{project.title}</h3>
-				<p class="text-sm md:text-base mb-4">{project.description}</p>
-				{#if project.link}
-					<a
-						href={project.link}
-						target="_blank"
-						rel="noopener noreferrer"
-						class="text-blue-500 hover:underline">View Project</a
-					>
-				{/if}
-			</div>
-		{/each}
-	</div> -->
 </section>
