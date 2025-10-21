3341a1b13f21adb3a192a11ed381378120c6c236
adrhmdlz <adrianalzidan35@gmail.com>
2025-10-21 16:49:56 +0700

    fix(button): fix button components reusable issue

--- src/lib/components/ui/button/Button.component.svelte
+++ src/lib/components/ui/button/Button.component.svelte
@@ -1,16 +1,16 @@
 <script>
-	let { type, href, label, class: className } = $props();
+	let { type, href, label, class: className, target = '_self' } = $props();
 
 	const baseClass =
 		`${className ? className : ''} ` +
-		'relative rounded-[40px] p-[1px] text-sm text-black text-center';
+		'relative rounded-[40px] md:p-[1px] text-sm text-black text-center';
 	const variantClass = {
-		primary: 'hover:bg-gradient-to-r from-[#fed39c] to-[#7a87fa]',
+		primary: 'md:hover:bg-gradient-to-r from-[#fed39c] to-[#7a87fa]',
 		secondary: 'border border-[#7a87fa] text-[#7a87fa] hover:bg-[#7a87fa] hover:text-white'
 	};
 </script>
 
-<a href={href || '#'} class={`${baseClass} ${variantClass[type]}`} target="_blank">
+<a href={href || '#'} class={`${baseClass} ${variantClass[type]}`} {target}>
 	<span
 		class={`block rounded-[40px] px-4 py-3 md:px-[20px] md:py-[14px] font-semibold ${type === 'primary' ? 'bg-gradient-to-r from-[#7a87fa] to-[#fed39c]' : 'text-white'}`}
 		>{label}</span
